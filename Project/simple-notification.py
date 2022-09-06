e83d35bb7c1dac2134e008d48fae37bb8d852a0a
Tushar <tusharbehera32@gmail.com>
2022-09-06 19:44:58 +0530

    Project on notification on process

---
+++ Project/simple-notification.py
@@ -0,0 +1,8 @@
+# import "plyer" to use for notification use --> pip install plyer
+from plyer import notification
+notification.notify( title="ORO - v 1.0 ",
+                    message="Sir your deployment for this notification is stable..!!\n& working properly 🤓 Lets upgrade into v2.0",
+                    app_icon= None,
+                    app_name="OrO Notifier",
+                    timeout=10,
+                    ) # do not miss comma while separating the keys
---
+++ Project/test.py
@@ -0,0 +1,6 @@
+x=input("please enter  y oy Y : ")
+if x== "Y" or "y":
+    x=True
+
+print(x)
+
